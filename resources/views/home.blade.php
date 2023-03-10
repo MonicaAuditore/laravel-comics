882fe6aae770bb2f81cd86bc6bf7df38d672ef8a
MonicaAuditore <mauditore@live.it>
2023-03-10 11:02:13 +0100

    fix_MediaQueries

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,9 @@
 <div class="container pt-5 pb-5">
     <div class="row g-4">
       @foreach($comics as $singleCard)
-      <div class="col-2 ">
+      <div class="col-lg-2 col-md-3 col-sm-3">
         <img style="width: 100%;" src="{{$singleCard['thumb']}}">
-        <h6 class="text-white testoCard pt-3">{{$singleCard['series']}}</h6>
+        <h6 style="font-size: 14px;" class="text-white testoCard pt-3">{{$singleCard['series']}}</h6>
       </div>
       @endforeach
     </div>
