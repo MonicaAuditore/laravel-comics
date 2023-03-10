22012ab2e2d8e3517265c030b8e25b0c8d9d2fbd
MonicaAuditore <mauditore@live.it>
2023-03-10 10:38:29 +0100

    fix_Bonus

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,17 +4,5 @@
 
 @section('content')
 
-<div class="all-cards">
-<div class="container pt-5 pb-5">
-    <div class="row g-4">
-      
-    </div>
-  </div>
-
-
-  <div class="button-card text-center pt-3 pb-4">
-    <button type="button" class="btn btn-primary btn-lg">LOAD MORE</button>
-
-  </div>
-  </div>
+<h1 class="pt-5 pb-5 text-center">Content News</h1>
 @endsection
